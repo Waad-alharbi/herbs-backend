a573e7c2b5f06fbbd6b75536b8b3a6d205f2b854
waad-alharbi <harbi.waad.s@gmail.com>
2025-05-01 01:05:35 +0300

    add herb detail view and app url path

--- herbs/urls.py
+++ herbs/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import  HerbListCreateView
+from .views import  HerbListCreateView, HerbDetailView
 
 
 urlpatterns = [
    
     path('herbs/', HerbListCreateView.as_view(), name='herb-list-create'),
+    path('herbs/<int:pk>/', HerbDetailView.as_view(), name='herb_detail'),
 
     
 ]
--- herbs/views.py
+++ herbs/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Herb
@@ -16,3 +16,12 @@ class  HerbListCreateView(APIView):
             serializer.save()
             return Response(serializer.data, status=201)
         return Response(serializer.errors, status=400)
+    
+class HerbDetailView(APIView):
+    def get_object(self, pk):
+        return get_object_or_404(Herb, pk=pk)
+    
+    def get(self, request, pk):
+        herb = self.get_object(pk)
+        serializer = HerbSerializer(herb)
+        return Response(serializer.data)
