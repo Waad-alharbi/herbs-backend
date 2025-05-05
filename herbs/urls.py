99595797ec1947253ef4bd58764bd086f9794ffd
waad-alharbi <harbi.waad.s@gmail.com>
2025-05-05 03:21:33 +0300

    adding authentication urls

--- herbs/urls.py
+++ herbs/urls.py
@@ -14,7 +14,9 @@ urlpatterns = [
     path('categories/', CategoryListAPIView.as_view(), name='category-list'),
     path('healthtracker/', HealthTrackerListCreateView.as_view(), name='health-tracker-list-create'),
     path('healthtracker/<int:pk>/', HealthlogDetailView.as_view(), name='health_log_detail'),
+   
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('signup/', SignUpView.as_view(), name='signup')
 
